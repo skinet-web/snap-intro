61c37634204382b87c0c9f8aac897d505b1016c9
skinet-web <skinet2009@gmail.com>
2023-03-05 23:23:12 +0200

    Updated the Navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,37 +1,52 @@
 import React, { useState } from 'react'
 import { logo, menu, close, arrowUp, arrowDown, toDoList, calendar, reminders, planning  } from '../assets'
 
-const DropDown = () => {
-    const [arrowDirection, setSetArrowDirection] = useState(false)
+
+const DropDown = ({className, navbar}) => {
+    
+    const [menuStates, setMenuStates] = useState({
+        features: false,
+        company: false
+    });
+
+    const toggleMenu = (menu) => {
+        setMenuStates(prevState => ({
+            ...prevState,
+            [menu]: !prevState[menu]
+        }));
+    };
+
     return (
-        <ul className='flex flex-col gap-5 text-white'>
-            <li className='flex flex-col'>
-                                    <div className='flex items-center'> 
+        <ul className={`${className} `} >
+            <li className='relative'>
+                                    <div 
+                                    className='flex items-center'
+                                    onClick={() => toggleMenu('features')}> 
                                         Features
                                         <img 
-                                        src={arrowDirection ? arrowDown : arrowUp} 
+                                        src={menuStates.features ? arrowDown : arrowUp} 
                                         className='w-2 h-2 ml-1 white--arrow'
-                                        onClick={() => setSetArrowDirection((prev) => !prev)}
+                                        
                                         />
                                     </div>                                   
-                                    {arrowDirection && (
-                                    <div className='flex flex-col  ml-7'>
+                                    {menuStates.features && ( 
+                                    <div className={`${navbar} sidebar--micro`}>
                                         <div className='flex flex-row items-center  '>                                    
                                             <img className='w-4 h-4 mr-2' 
                                             alt="to do icon" src={toDoList} /> 
                                             <p>Todo List</p>
                                         </div>
-                                        <div className='flex flex-row items-center  '>                                    
+                                        <div className='flex flex-row items-center mt-1 '>                                    
                                             <img className='w-4 h-4 mr-2' 
                                             alt="calendar icon" src={calendar} /> 
                                             <p>Calendar</p>
                                         </div>
-                                        <div className='flex flex-row items-center  '>                                    
+                                        <div className='flex flex-row items-center mt-1 '>                                    
                                             <img className='w-4 h-4 mr-2' 
                                             alt="reminders icon" src={reminders} /> 
                                             <p>Reminders</p>
                                         </div>
-                                        <div className='flex flex-row items-center  '>                                    
+                                        <div className='flex flex-row items-center mt-1 '>                                    
                                             <img className='w-4 h-4 mr-2' 
                                             alt="planning icon" src={planning} /> 
                                             <p>Planning</p>
@@ -42,64 +57,95 @@ const DropDown = () => {
                                     
                                     )}
             </li> 
-            <li>Company</li> 
-            <li>Careers</li> 
-            <li>About</li> 
+            <li className='relative' >
+                                        <div 
+                                        className='flex items-center'
+                                        onClick={() => toggleMenu('company')}> 
+                                            Company
+                                            <img 
+                                            src={menuStates.company ? arrowDown : arrowUp} 
+                                            className='w-2 h-2 ml-1 white--arrow'
+                                            
+                                            />
+                                        </div>                                   
+                                        {menuStates.company && ( 
+                                        <div className={`${navbar} sidebar--micro`}>
+                                            <div className='flex flex-row items-center mt-1 '>                                    
+                                                <p>History</p>
+                                            </div>
+                                            <div className='flex flex-row items-center mt-1 '>                                    
+                                                <p>Our Team</p>
+                                            </div>
+                                            <div className='flex flex-row items-center  mt-1'>                                    
+                                                <p>Blog</p>
+                                            </div>
+                                            
+                                        </div>
+                                        
+                                        
+                                        
+                                        )}
+            </li> 
+            <li className='' >Careers</li> 
+            <li className='' >About</li> 
         </ul> 
     )
 
 }
 
 const Navbar = () => {
-    const [toggle, setToggle] = useState(false)
-    const [toggleToDo, setToggleToDo] = useState(false)
-    
+    const [toggle, setToggle] = useState(false)    
 
   return (
     <nav className='flex justify-between items-center font-poppins'>
         <div className='justify-evenly items-center sm:flex hidden'>
             <div className='flex justify-evenly'>
-                <img src={logo} alt='logo' className='w-[100%] h-[100%] m-5'/>
+                <img src={logo} alt='logo' className='w-[100%] h-[100%] mt-6 m-5'/>
             </div>
-            <ul className='flex flex-row gap-8 ml-10 mt-'>
-                    <li>Features</li> 
-                    <li>Company</li> 
-                    <li>Careers</li> 
-                    <li>About</li> 
-            </ul>       
+            <DropDown 
+            className='flex flex-row gap-10 ml-8' 
+            navbar = 'absolute top-full left-0 mt-2 w-[500px] items-center' 
+            />
+          
         </div>
         <div className='flex-row sm:flex hidden gap-8 mr-10 font-poppins items-center'>
-                <p className=''>Login</p>
+                <button className=''>Login</button>
                 <button className='border-2 rounded-xl border-black p-2'>Register</button>
         </div>
 
 
         {/* Mobile device */}
-        <div className='sm:hidden flex flex-1 justify-end items-center font-poppins'>
-          <img 
-            src={toggle ? close : menu} 
-            alt='menu'
-            className='w-[28px] h-[28px]  object-contain'
-            onClick={() => setToggle((prev) => !prev)}      
-          />               
-           <div 
-            className={`
-            ${toggle ? 'flex' : 'hidden'}
-            p-6 bg-black absolute top-5 right-0
-            mx-4 my-2 min-w-[140px] rounded-xl sidebar
-            flex-col`}
-            > 
-                
-            <DropDown />
-                        
-                        
-            <ul className='flex flex-col items-start text-white gap-5 mt-10'>
-                <li>Login</li>
-                <button>Register</button>
-            </ul>
+            <div className='sm:hidden flex justify-evenly'>
+                <img src={logo} alt='logo' className='w-[100%] h-[100%] mt-6 m-5'/>
             </div>
+            <div className='sm:hidden flex flex-1 p-5 justify-end items-center font-poppins'>
+                <img 
+                    src={toggle ? close : menu} 
+                    alt='menu'
+                    className='w-[30px] h-[20%]  object-contain'
+                    onClick={() => setToggle((prev) => !prev)}      
+                />               
+                <div 
+                    className={`
+                    ${toggle ? 'flex' : 'hidden'}
+                    p-6 bg-black absolute top-10 right-0
+                    mx-4 my-2 min-w-[140px] rounded-xl sidebar
+                    flex-col`}
+                    > 
+                        
+                    <DropDown 
+                    className='flex flex-col gap-5 text-white'
+                    navbar = 'flex flex-col  ml-7'
+                    />
+                                
+                                
+                    <ul className='flex flex-col items-start text-white gap-5 mt-10'>
+                        <li>Login</li>
+                        <button>Register</button>
+                    </ul>
+                </div>
 
-        </div>   
+            </div>   
 
     </nav>
   )
